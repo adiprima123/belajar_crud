d76f8e55bd0056c3b44b01d30acc307248e76e2d
adiprima123 <adiprimavoc123@gmail.com>
2022-08-05 07:04:12 -0800

    tambah koneksi

--- belajarcrud5.php
+++ belajarcrud5.php
@@ -6,10 +6,9 @@
         <h2>CRUD DATA PERUSAHAAN</h2>
         
 <br/>
+<h3>ANGGOTA</h3>
 <a href= "tambah.php">tambah data</a>
 <br/>
-<br/>
-<h3>anggota</h3>
 <table border="2">
     <tr>
         <th>no</th>
@@ -20,15 +19,7 @@
     </tr>
 
 <?php
-//belajar koneksi database
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "db_perpusweb";
-
-//membuat koneksi
-$connect = mysqli_connect($servername,$username,$password,$database);
-
+include 'koneksi.php';
 $no= 1;
 $data = mysqli_query($connect,"SELECT * FROM anggota");
 while($d = mysqli_fetch_array($data)) {
@@ -42,9 +33,99 @@ while($d = mysqli_fetch_array($data)) {
     <var_dump($d)?>
 </tr>
 <?php
+
+}
+?>
+<br/>
+</table>
+<br/>
+<br/>
+<h3>ADMIN</h3>
+<a href= "tambah.php">tambah data</a>
+<br/>
+<br/>
+<table border="2">
+    <tr>
+        <th>no</th>
+        <th>username</th>
+        <th>password</th>
+
+    </tr>
+
+<?php
+//belajar koneksi database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "db_perpusweb";
+
+//membuat koneksi
+$connect = mysqli_connect($servername,$username,$password,$database);
+
+$no= 1;
+$data = mysqli_query($connect,"SELECT * FROM admin");
+while($d = mysqli_fetch_array($data)) {
+?>
+<tr>
+    <td><?php echo $no++ ?></td>
+    <td><?php echo $d['username']; ?></td>
+    <td><?php echo $d['password']; ?></td>
+    <var_dump($d)?>
+</tr>
+<?php
     
 }
 ?>
+
+</table>
+    
+<br/>
+</table>
+<br/>
+<br/>
+<h3>BOOK</h3>
+<a href= "tambah.php">tambah data</a>
+<br/>
+<br/>
+<table border="2">
+    <tr>
+        <th>no</th>
+        <th>id_katalog</th>
+        <th>judul buku</th>
+        <th>pengarang</th>
+        <th>tahun terbit</th>
+        <th>penerbit</th>
+
+    </tr>
+
+<?php
+//belajar koneksi database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "db_perpusweb";
+
+//membuat koneksi
+$connect = mysqli_connect($servername,$username,$password,$database);
+
+$no= 1;
+$data = mysqli_query($connect,"SELECT * FROM book");
+while($d = mysqli_fetch_array($data)) {
+?>
+<tr>
+    <td><?php echo $no++ ?></td>
+    <td><?php echo $d['id_katalog']; ?></td>
+    <td><?php echo $d['judul_buku']; ?></td>
+    <td><?php echo $d['pengarang']; ?></td>
+    <td><?php echo $d['thn_terbit']; ?></td>
+    <td><?php echo $d['penerbit']; ?></td>
+    <var_dump($d)?>
+</tr>
+<?php
+    
+}
+?>
+
 </table>
 </body>
 </html>
